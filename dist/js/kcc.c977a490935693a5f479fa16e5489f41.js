69d533b14060d48e71baaeed038bf026690c4a19
question13 <robert@westworldentertainment.com>
2021-08-05 22:19:54 -0700

    build

---
+++ dist/js/kcc.c977a490935693a5f479fa16e5489f41.js
@@ -0,0 +1,30 @@
+/*!
+* YieldFarming
+* Boilerplate for a Static website using EJS and SASS
+* https://yieldfarming.info
+* @author Jongseung Lim -- https://yieldfarming.info
+* Copyright 2021. MIT Licensed.
+*/
+
+$(function() {
+  main()
+});
+
+const main = async() => {
+
+  let tableData = {
+    "title":"KCC Network",
+    "heading":["Pool Provider","LP", "Reward Tokens", "INFO"],
+    "rows": [
+      [ "Kudex",      `<a href="kudex"        >Various</a>`,     "KUD",       "https://kudex.finance" ],
+      [ "KuSwap",     `<a href="kuswap"       >Various</a>`,     "KUS",       "https://kuswap.io" ],
+      [ "KuCat",      `<a href="kucat"        >Various</a>`,     "CAT",       "https://kucat.finance" ],
+      [ "KuBeans",    `<a href="kubeans"      >Various</a>`,     "KUBEANS",   "https://kubeans.club" ],
+      [ "KandySwap",  `<a href="kandyswap"    >Various</a>`,     "KANDY",     "https://www.kandyswap.com" ]
+    ]
+  }
+
+  let table = new AsciiTable().fromJSON(tableData);
+  document.getElementById('log').innerHTML += table + '<br />';
+  hideLoading();
+}
